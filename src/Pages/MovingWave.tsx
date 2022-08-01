bdaf686c52dcd616118d3a9c02b34753a9f690d2
nyk <nykoh2001@gmail.com>
2022-08-01 16:19:46 +0900

    refactoring: 경로 수정

--- src/Pages/MovingWave.tsx
+++ src/Pages/MovingWave.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "../Component/style.css";
-import "../Component/app.tsx";
+import "../Component/moving_wave/style.css";
+import "../Component/moving_wave/app.tsx";
 
 const MovingWave = () => {
   return <div></div>;
